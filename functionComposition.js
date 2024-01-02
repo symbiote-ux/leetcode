1c7ea8a40a78bdb2a58fa472e6ba1543ee45b710
Saurabh Chauhan <tnikhil422@gmail.com>
2024-01-02 19:08:58 +0530

    Add FunctionComposition method

---
+++ functionComposition.js
@@ -0,0 +1,20 @@
+var compose = function (functions) {
+  return function (x) {
+    let result = x;
+    for (let i = functions.length - 1; i >= 0; i--) {
+      result = functions[i](result);
+    }
+    return result;
+  };
+};
+
+/*Example 1:
+
+Input: functions = [x => x + 1, x => x * x, x => 2 * x], x = 4
+Output: 65
+Explanation:
+Evaluating from right to left ...
+Starting with x = 4.
+2 * (4) = 8
+(8) * (8) = 64
+(64) + 1 = 65  */
